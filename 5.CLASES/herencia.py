bca11818aca9d20e12f9287880008da97f8f383f
CrisLeonGitHub <cleonfuentes@gmail.com>
2024-05-07 22:42:13 -0400

    modificacion

--- 5.CLASES/herencia.py
+++ 5.CLASES/herencia.py
@@ -12,5 +12,25 @@ class Empleado(Persona):
         self.trabajo = trabajo
         self.salario = salario
 
+class Artista:
+    def __init__(self, habilidad):
+        self.habilidad = habilidad
+        
+    def mostrar_habilidad(self):
+        print(f"Mi habilidad es {self.habilidad}")
+        
+class EmpleadoArtista(Persona,Artista):
+    def __init__(self, nombre , edad , nacionalidad,habilidad, salario,empresa):
+        Persona.__init__(self, nombre , edad , nacionalidad)
+        Artista.__init__(self,habilidad)
+        self.salario = salario
+        self.empresa = empresa
+    
+    def presentarse(self): 
+        return f'{super().mostrar_habilidad()}'
+    
 ems = Empleado("Cris",23,"chileno","teachleader",2200000)
 ems.hablar()
+
+artis = EmpleadoArtista("arjona",34,"la que sea","cantante",23000,"Sony")
+artis.presentarse()
